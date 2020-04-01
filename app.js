6f0ee2c228854a52e80c97ded295525af573d9ca
Nina Potrebich <potrebich@gmail.com>
2020-04-01 14:30:09 -0700

    Read input data

--- app.js
+++ app.js
@@ -8,22 +8,49 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    //some code
+
+    var DOMstrings = {
+        inputTypes: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputTypes).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
+
 })();
 
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
+
         // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget 
         // 5. Display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
         
