872941e96148c8874e4326daae5c8a6654948bc1
Nina Potrebich <potrebich@gmail.com>
2020-04-01 14:08:10 -0700

    Add first event listeners

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+
+//BUDGET CONTROLLER
 var budgetController = (function() {
     
-    var x = 23;
 
-    var add = function(a){
-        return x + a;
-    };
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
     //some code
 })();
 
+
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        // 1. Get the filed input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget 
+        // 5. Display the budget on the UI
     }
+
+    document.querySelector('.add__btn').addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
