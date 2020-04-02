4f95325b3930ab0d3bd164729289fbe1b58f4efa
Nina Potrebich <potrebich@gmail.com>
2020-04-02 13:48:11 -0700

    Add initialization

--- app.js
+++ app.js
@@ -36,7 +36,21 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+        document.addEventListener("keypress", function(event) {
+            
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    
 
     var ctrlAddItem = function() {
 
@@ -48,15 +62,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         // 4. Calculate the budget 
         // 5. Display the budget on the UI
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-    document.addEventListener("keypress", function(event) {
-        
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log("Application has started");
+            setupEventListeners();
         }
-    })
+    };
 
 })(budgetController, UIController);
+
+controller.init();
